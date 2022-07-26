6d36c0279c0d61955ff497a84b25bd0e97140716
Eduardo <eduarduholiveira@gmail.com>
2022-07-26 19:21:18 -0300

    exercicio feedback aula09

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nomeUsuario = prompt("Qual seu nome ?");
+const comidaFavorita1 = prompt ("Qual a sua comida favorita ?");
+const comidaFavorita2 = prompt ("Qual a sua comida favorita ?");
+const comidaFavorita3 = prompt ("Qual a sua comida favorita ?");
+
+console.log(nomeUsuario,comidaFavorita1,comidaFavorita2,comidaFavorita3);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou superticioso, mas sou um pouco________________ .        ";
+
+const semEspaco = minhaString.trim();
+
+console.log(semEspaco,semEspaco.length);
+const substitui = minhaString.replace("________________"," sticioso");
+console.log(substitui);
+
+console.log(substitui.length);
